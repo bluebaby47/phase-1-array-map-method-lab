0121cc4a23e7f9aae39d02d8061fed1196dba94b
kevin ikahu <kevinikahu4@gmail.com>
2023-03-30 16:07:46 -0700

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial =>
+    tutorial.replace(/\b\w+/g, word => word.charAt(0).toUpperCase() + word.slice(1))
+  );
+};
